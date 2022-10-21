738c64e6e286f236baa1cbed95a25e4fbd103f76
Anthony Zhu <anthonyzhu132@gmail.com>
2022-10-20 22:28:38 -0700

    added routing

---
+++ pages/admin/index.tsx
@@ -0,0 +1,7 @@
+export default function AdminPostPage({}) {
+  return (
+    <main>
+      <h1>Admin page</h1>
+    </main>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-
+      <Link href="/test123">
+        <a>profile</a>
+      </Link>
     </div>
-  )
+  );
 }
