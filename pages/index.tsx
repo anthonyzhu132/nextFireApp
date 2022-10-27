299b74a5696b622579b5c1e4d3a4404260595643
Anthony Zhu <anthonyzhu132@gmail.com>
2022-10-26 21:12:28 -0700

    added navbar and loader component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 
+import Loader from "../components/Loader";
+
 export default function Home() {
   return (
     <div>
-      <Link href="/test123">
-        <a>profile</a>
-      </Link>
+      <Loader show />
     </div>
   );
 }
