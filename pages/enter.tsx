036f369c0563c2a1617f23fb3d2b9972d00cf1ab
Anthony Zhu <anthonyzhu132@gmail.com>
2022-10-26 22:08:16 -0700

    added google sign on with adjiusting version number for firebase

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,7 +1,39 @@
-export default function EnterPage({}) {
+import { auth, googleAuthProvider } from "../lib/firebase";
+
+export default function Enter(props) {
+  const user = null;
+  const username = null;
   return (
     <main>
-      <h1>Sign up</h1>
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
     </main>
-  )
+  );
+}
+
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    await auth.signInWithPopup(googleAuthProvider);
+  };
+
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src="../public/google.png" alt="Google" /> Sign in with Google
+    </button>
+  );
+}
+
+function SignOutButton() {
+  return <button onClick={() => auth.signOut()}>Sign Out</button>;
+}
+
+function UsernameForm() {
+  return null;
 }
